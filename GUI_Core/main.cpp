e5fb90945b79d7328b6db561613987dd185cc36e
EasyToFindName <golodorov@gmail.com>
2018-07-21 23:45:14 +0300

    Implemented drag and drop

--- GUI_Core/main.cpp
+++ GUI_Core/main.cpp
@@ -6,10 +6,12 @@
 #include "CenterLayout.h"
 #include "DefaultButton.h"
 #include "Container.h"
+#include "DragAndDrop.h"
 
 
 #include <SFML/System.hpp>
 #include <iostream>
+#include <stack>
 
 #define FONT_ARIAL "./res/fonts/arial.ttf"
 
@@ -21,30 +23,39 @@ int main() {
 	gui.setBackgroundImage("./res/textures/bg.png");
 	gui.setCursor("./res/cursors/Cursor.png", 32, 32);
 
-	auto main = gui.addElem<VerticalLayout>(gui.getSize().x, gui.getSize().y);
-	main->setPadding(25.0f);
-	main->setOffset(30.0f);
+	//header
+	auto controlsWrapper = gui.addElem<CenterLayout>(gui.getSize().x, gui.getSize().y / 6);
+	controlsWrapper->setFillColor(sf::Color(192, 192, 192, 192));
 	
-	auto button = main->addElem<DefaultButton>(200.0f, 70.0f);
-	button->setText(FONT_ARIAL, "Add button");
+	auto controls = controlsWrapper->addElem<HorizontalLayout>(controlsWrapper->getSize().x / 2, controlsWrapper->getSize().y);
+	controls->setOffset(24.0f);
 
-	button->addListener(GuiActiveElem::PRESSED, [main]() {
-		auto* useless = main->addElem<DefaultButton>(200.0f, 70.0f);
-		useless->setText(FONT_ARIAL, "Click to remove");
+	auto createButton = controls->addElem<DefaultButton>(200.0f, 70.0f);
+	createButton->setText(FONT_ARIAL, "Create button");
 
-		useless->addListener(GuiActiveElem::MOUSE_OVER, [useless]() {
-			useless->setText(FONT_ARIAL, "Don't kill me pls!");
-		});
+	auto deleteButton = controls->addElem<DefaultButton>(200.0f, 70.0f);
+	deleteButton->setText(FONT_ARIAL, "Delete button");
 
-		useless->addListener(GuiActiveElem::MOUSE_OUT, [useless]() {
-			useless->setText(FONT_ARIAL, "Click to remove");
-		});
+	//main
+	std::stack<DefaultButton*> mainButtons;
 
-		useless->addListener(GuiActiveElem::PRESSED, [main, useless]() {
-			main->removeElem(useless);
-		});
+	auto main = gui.addElem<Container>();
+	main->setPosition(gui.getSize().x / 3, gui.getSize().y / 4);
+	
+	createButton->addListener(GuiActiveElem::PRESSED, [&mainButtons, main](const sf::Event& e) mutable {
+		auto draggableButton = main->addElem<DefaultButton>(200.0f, 70.0f);
+		DragAndDrop::apply(draggableButton);
+		draggableButton->setText(FONT_ARIAL, "Drag me");
+		mainButtons.push(draggableButton);
 	});
 
+	deleteButton->addListener(GuiActiveElem::PRESSED, [&mainButtons, main](const sf::Event& e) mutable {
+		if (mainButtons.size()) {
+			auto toDelete = mainButtons.top();
+			mainButtons.pop();
+			main->removeElem(toDelete);
+		}
+	});
 
 	while (window.isOpen()) {
 		gui.run(sf::Time::Zero);
